992191fb16a1ee387f8fd58cb659171bb83c1bc1
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-06-20 09:25:52 +0300

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let arr = word.split('')
+  arr = arr.reverse()
+  arr = arr.join('')
+  return arr === word
 }
 
+
+// console.log(isPalindrome('racecar'))
+// /home/purity/Desktop/moringa/phase-1/phase-1-algorithms-palindrome/index.js
 /* 
-  Add your pseudocode here
+  loop through string forward
+  assign the looped outcome to a variable
+  loop through string backwards
+  compare current outcome with previous variable
+  if similar return true
+  if not return false
+
+  reverse the string
+  compare the reversed string with the original string
+  if similar, return true, else return false
 */
 
 /*
